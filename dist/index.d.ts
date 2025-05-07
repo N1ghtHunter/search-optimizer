129e02fd62982fee86b4b9cc61792b55564eb629
Mazin Islam <mazin.islam@robustastudio.com>
2025-05-07 11:26:14 +0300

    chore: remove dist folder from git tracking

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { createSearchOptimizer } from './core';
-export type { SearchExecutor, SearchOptimizerOptions, SearchOptimizerResult } from './types/index';
-export * from './types';
-export { createFetchExecutor } from './adapters/fetch';
-export { createAxiosExecutor } from './adapters/axios';
-export { createGraphQLExecutor } from './adapters/graphql';
-export { useSearchOptimizer } from './adapters/react';
-export { debounce, processInput, shouldPerformSearch } from './utils';
